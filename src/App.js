793c5fe4290b47565d385836839aef338e875601
Hasan Cheraghi <hasan.cheraghi1994@gmail.com>
2021-10-27 12:06:35 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { useState } from 'react';
 
 function App() {
+  //your google client id here ------------ you can get client id from this link  https://console.cloud.google.com/apis/credentials
+  const ClientID = "your-client-key-here";
+
+  const [email, setEmails] = useState('');
+  const [nameFamily, setnameFamily] = useState('');
+  const [profileImgUrl, setprofileImgUrl] = useState('');
+
+  const responseGoogle = (response) => {
+    if (response.profileObj) {
+      const obj = response.profileObj;
+      setEmails(obj.email);
+      setnameFamily(obj.name);
+      setprofileImgUrl(obj.imageUrl);
+    }
+    console.log(`response is ${response.tokenId}`, `tokenObj`, `profileObj ${response.profileObj.name} ${response.profileObj.email} ${response.profileObj.imageUrl}`);
+  }
+
+  const logoutGoogle = (resp) => {
+    setEmails('')
+    setnameFamily('')
+    setprofileImgUrl('')
+    alert('logout successfuly')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <GoogleLogin
+        clientId={ClientID}
+        buttonText="Login with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        isSignedIn={true} />
+
+      <GoogleLogout
+        clientId={ClientID}
+        buttonText="Logout Google"
+        onLogoutSuccess={logoutGoogle}
+      />
+
+      <div>
+        <h1>Login info</h1>
+        <img src={profileImgUrl} alt={nameFamily} />
+        <h3>{`email is: ${email}`}</h3>
+        <h3>{`name family is: ${nameFamily}`}</h3>
+      </div>
+    </div >
   );
 }
 
